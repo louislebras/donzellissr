5c8027e955534baaa57750d9dc6e740e4c6d92a3
Louis Le Bras <louislebras@MacBook-Pro-de-Louis.local>
2024-01-04 19:35:05 +0100

    final product

--- js/product-header.js
+++ js/product-header.js
@@ -1,79 +1,3 @@
-// window.addEventListener("scroll", () => {
-//   // Calculez la distance de défilement en tant que pourcentage de la hauteur de la vue
-//   const scrollDistance = window.scrollY / window.innerHeight;
-
-//   // Obtenez l'élément d'en-tête par son sélecteur (ajustez le sélecteur selon votre besoin)
-//   const header = document.querySelector("header");
-
-//   // Vérifiez si la distance de défilement est supérieure à 270vh (2.7 fois la hauteur de la vue)
-//   if (scrollDistance < 0.8) {
-//     // Ajoutez la classe souhaitée à l'élément d'en-tête
-//     header.classList.add("background-product-page-header");
-//   } else {
-//     // Supprimez la classe si la distance de défilement est inférieure à 270vh
-//     header.classList.remove("background-product-page-header");
-//   }
-// });
-
-// window.addEventListener("scroll", () => {
-//   // Calculez la distance de défilement en tant que pourcentage de la hauteur de la vue
-//   const scrollDistance = window.scrollY / window.innerHeight;
-
-//   // Obtenez l'élément d'en-tête par son sélecteur (ajustez le sélecteur selon votre besoin)
-//   const headerM = document.querySelector(".header-mobile");
-
-//   // Vérifiez si la distance de défilement est supérieure à 270vh (2.7 fois la hauteur de la vue)
-//   if (scrollDistance < 0.5) {
-//     // Ajoutez la classe souhaitée à l'élément d'en-tête
-//     headerM.classList.add("background-product-page-headerM");
-//   } else {
-//     // Supprimez la classe si la distance de défilement est inférieure à 270vh
-//     headerM.classList.remove("background-product-page-headerM");
-//   }
-// });
-
-// let isMouseOverHeaderOrContainer = false;
-
-// // Ajoutez un écouteur d'événements pour le survol sur l'en-tête
-// document.querySelector("header").addEventListener("mouseover", () => {
-//   isMouseOverHeaderOrContainer = true;
-//   document
-//     .querySelector("header")
-//     .classList.remove("background-product-page-header");
-// });
-
-// // Ajoutez un écouteur d'événements pour le survol sur .container-panels
-// document
-//   .querySelector(".container-panels")
-//   .addEventListener("mouseover", () => {
-//     isMouseOverHeaderOrContainer = true;
-//   });
-
-// // Ajoutez un écouteur d'événements pour le mouseout sur l'en-tête
-// document.querySelector("header").addEventListener("mouseout", () => {
-//   isMouseOverHeaderOrContainer = false;
-//   // Utilisez setTimeout pour donner le temps à la souris de passer à .container-panels
-//   setTimeout(() => {
-//     if (!isMouseOverHeaderOrContainer) {
-//       document
-//         .querySelector("header")
-//         .classList.add("background-product-page-header");
-//     }
-//   }, 10);
-// });
-
-// // Ajoutez un écouteur d'événements pour le mouseout sur .container-panels
-// document.querySelector(".container-panels").addEventListener("mouseout", () => {
-//   isMouseOverHeaderOrContainer = false;
-//   // Utilisez setTimeout de la même manière que pour l'en-tête
-//   setTimeout(() => {
-//     if (!isMouseOverHeaderOrContainer) {
-//       document
-//         .querySelector("header")
-//         .classList.add("background-product-page-header");
-//     }
-//   }, 10);
-// });
 let isMouseOverHeaderOrContainer = false;
 
 // Fonction pour mettre à jour la classe de l'en-tête
@@ -112,3 +36,20 @@ document.querySelectorAll("header, .container-panels").forEach((element) => {
 
 // Gestionnaire d'événements de défilement
 window.addEventListener("scroll", updateHeaderClass);
+
+window.addEventListener("scroll", () => {
+  // Calculez la distance de défilement en tant que pourcentage de la hauteur de la vue
+  const scrollDistance = window.scrollY / window.innerHeight;
+
+  // Obtenez l'élément d'en-tête par son sélecteur (ajustez le sélecteur selon votre besoin)
+  const headerM = document.querySelector(".header-mobile");
+
+  // Vérifiez si la distance de défilement est supérieure à 270vh (2.7 fois la hauteur de la vue)
+  if (scrollDistance < 0.5) {
+    // Ajoutez la classe souhaitée à l'élément d'en-tête
+    headerM.classList.add("background-product-page-headerM");
+  } else {
+    // Supprimez la classe si la distance de défilement est inférieure à 270vh
+    headerM.classList.remove("background-product-page-headerM");
+  }
+});
